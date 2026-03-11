424cf9ca8bfe1f70fe43b0c3d2bb109de761aa0c
Murilo Dutra <murilohcanoas@gmail.com>
2026-03-11 17:58:09 -0300

    primeiro exercicio java

---
+++ exercicio1.java
@@ -0,0 +1,13 @@
+public class {
+    public static void main (String [] args) {
+
+    int num = 10; 
+
+    if (num % 2 == 0) {
+System.out.println ("Número par!"); 
+   } else { 
+System.out.println ("Número não é par!");
+}
+}
+} 
+
